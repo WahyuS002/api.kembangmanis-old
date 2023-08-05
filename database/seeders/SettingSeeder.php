3fdd61d5d4b9f2b80fd2f8a70eaaec2d842234f6
WahyuS002 <syahputrawahyu61@gmail.com>
2023-08-05 21:53:29 +0700

    feat: add SettingSeeder

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            'siteName' => 'Desa Kembang Manis',
+            'siteDescription' => 'Website Desa Kembang Manis',
+            'structureImage' => 'struktur-pemerintah.png', // Replace this with the image filename
+        ];
+
+        foreach ($data as $key => $value) {
+            if ($key === 'structureImage') {
+                $setting = Setting::create([
+                    'key' => $key,
+                    'value' => $value,
+                ]);
+
+                $setting->addMedia(public_path('images/' . $value))
+                    ->preservingOriginal()
+                    ->toMediaCollection('structure');
+            } else {
+                DB::table('settings')->insertOrIgnore([
+                    'key' => $key,
+                    'value' => $value,
+                ]);
+            }
+        }
+    }
+}
