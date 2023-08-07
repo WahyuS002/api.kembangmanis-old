719f6d2c81d94907e08acf96080d7ee3b6328823
WahyuS002 <syahputrawahyu61@gmail.com>
2023-08-07 14:02:03 +0700

    feat: add slug on News

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,15 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class News extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory, InteractsWithMedia, HasSlug;
 
     protected $fillable = ['title', 'content', 'author_id'];
+
+    protected $hidden = ['author_id'];
 
     public function author()
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
     }
 }
--- database/migrations/2023_08_06_151435_create_news_table.php
+++ database/migrations/2023_08_06_151435_create_news_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('news', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->text('content');
             $table->unsignedBigInteger('author_id');
             $table->timestamps();
